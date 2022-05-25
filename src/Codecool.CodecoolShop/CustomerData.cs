48704b13d823ea3241ef60fd95bee6028726b092
László Vargha <varghalaszlo90@gmail.com>
2022-05-25 18:37:12 +0200

    - refactor code, - create Util class - create CustomerData class

---
+++ src/Codecool.CodecoolShop/CustomerData.cs
@@ -0,0 +1,17 @@
+﻿namespace Codecool.CodecoolShop
+{
+    public class CustomerData
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string BillingCountry { get; set; }
+        public string BillingCity { get; set; }
+        public string BillingZip { get; set; }
+        public string BillingAddress { get; set; }
+        public string ShippingCountry { get; set; }
+        public string ShippingCity { get; set; }
+        public string ShippingZip { get; set; }
+        public string ShippingAddress { get; set; }
+    }
+}
